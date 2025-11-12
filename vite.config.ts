ceda4dd711f003481ee1bcc6bbc1149c30837bc4
shezalfatima <152189556+shezalfatima@users.noreply.github.com>
2025-11-12 22:34:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import path from 'path';import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
+  },
+  // 👇 This line is VERY important for Vercel!
+  base: "./",
+  build: {
+    outDir: "dist", // default, but good to be explicit
   },
 });
